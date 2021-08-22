be7f0274b0128577ac0479c2ece6cccbb6058ba6
Marco Wittig <Marco-Wittig@gmx.de>
2021-08-22 17:14:02 +0200

    Test freshly released wavefront reader
    Reading an obj file for a plane and render it

--- src/main/java/com/github/mwttg/jsget/EntityHelper.java
+++ src/main/java/com/github/mwttg/jsget/EntityHelper.java
@@ -5,6 +5,7 @@ import com.github.mwttg.sjge.graphics.draw.TextureLoader;
 import com.github.mwttg.sjge.graphics.draw.VboDefinition;
 import com.github.mwttg.sjge.graphics.draw.VertexArrayObject;
 import com.github.mwttg.sjge.graphics.draw.VertexBufferObject;
+import io.github.mwttg.wavefront.WavefrontReader;
 import org.lwjgl.opengl.GL40;
 
 import java.io.IOException;
@@ -12,38 +13,26 @@ import java.util.List;
 
 public final class EntityHelper {
 
-    private EntityHelper() {
-    }
+  private EntityHelper() {
+  }
 
-    public static Entity createDefaultPlane() {
-        final var geometry = new float[] {
-                1.0f, -1.0f, 0.0f,
-                -1.0f, 1.0f, 0.0f,
-                -1.0f, -1.0f, 0.0f,
-                1.0f, -1.0f, 0.0f,
-                1.0f, 1.0f, 0.0f,
-                -1.0f, 1.0f, 0.0f};
-        final var uv = new float[] {
-                1.0f, 0.0f,
-                0.0f, 1.0f,
-                0.0f, 0.0f,
-                1.0f, 0.0f,
-                1.0f, 1.0f,
-                0.0f, 1.0f};
-        try {
-            final var geometryVboId = VertexBufferObject.create(geometry, GL40.GL_STATIC_DRAW);
-            final var uvVboId = VertexBufferObject.create(uv, GL40.GL_STATIC_DRAW);
-            final var definition = List.of(
-                    new VboDefinition(geometryVboId, 3),
-                    new VboDefinition(uvVboId, 2));
-            final var vaoId = VertexArrayObject.create(definition);
-            final var textureId = TextureLoader.createFrom("textures/simple.png");
-            final var size = geometry.length / 3;
-            final var entity = new Entity(vaoId, textureId, size);
-            entity.moveTo(0,0, 0);
-            return entity;
-        } catch (final IOException e) {
-            throw new RuntimeException(e);
-        }
+  public static Entity createDefaultPlane() {
+    try {
+      final var planeFile = "wavefronts/plane.obj";
+      final var wavefront = WavefrontReader.fromResource(planeFile);
+      final var geometryVboId = VertexBufferObject.create(wavefront.vertices(), GL40.GL_STATIC_DRAW);
+      final var uvVboId = VertexBufferObject.create(wavefront.textureCoordinates(), GL40.GL_STATIC_DRAW);
+      final var definition = List.of(
+         new VboDefinition(geometryVboId, 3),
+         new VboDefinition(uvVboId, 2));
+      final var vaoId = VertexArrayObject.create(definition);
+      final var textureId = TextureLoader.createFrom("textures/simple.png");
+      final var size = wavefront.vertices().length / 3;
+      final var entity = new Entity(vaoId, textureId, size);
+      entity.moveTo(0, 0, 0);
+      return entity;
+    } catch (final IOException e) {
+      throw new RuntimeException(e);
     }
+  }
 }
